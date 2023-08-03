bbd4fd8d15036bfa17dbd3c939361c5408d2aaf9
pamelajoyyce-ul <lacsonpamelajoyce@gmail.com>
2023-08-03 16:59:36 +0800

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import "../src/index.css";
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  decorators: [mswDecorator],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+ import './index.css';
+ import store from './lib/store';
+
+ import { Provider } from 'react-redux';
+ import InboxScreen from './components/InboxScreen';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Provider store={store}>
+     <InboxScreen />
+   </Provider>
+  )
+}
+
+export default App
